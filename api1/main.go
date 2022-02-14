aa22470c32b0385be076f594bb3541ff5a48056e
Brandon Vicedomini <brandonvio@outlook.com>
2022-02-14 00:02:19 +0000

    Refactoring.

--- api1/main.go
+++ api1/main.go
@@ -4,20 +4,12 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("vim-go")
 	router := gin.Default()
-	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
-	config.AllowHeaders = []string{"Origin"}
-	config.ExposeHeaders = []string{"Content-Length"}
-	config.AddAllowMethods("OPTIONS")
-	config.AllowCredentials = true
-	// router.Use(cors.New(config))
 	router.Use(CORSMiddleware())
 	router.GET("/albums", apiGetAlbums)
 	router.Run()
